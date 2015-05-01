d9fff0bcf78fbf31f436b89394fc2ab7dc5b65d5
Dean Landolt <dean@deanlandolt.com>
2015-05-01 18:23:40 -0400

    Handle some errors

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ var JSONStream = require('JSONStream');
 var multiplex = require('multiplex');
 var Promise = require("bluebird");
 var reconnect = require('reconnect-engine');
-var split = require('split');
+var split2 = require('split2');
 
-var JSON_MODE_RE = /^application\/json(;.*(parse)(=([^;]+)?)?)?/;
+//
+// regex for sniffing content type for JSON/JSONStream-capable responses
+//
+var OBJ_MODE_RE = /^application\/json(;.*(parse)(=([^;]+)?)?)?/;
 
+//
+// returns an api client with an API that mirrors `endo`
+//
 module.exports = function (url, options) {
   // TODO: discover ws prefix?
   url = (url || '') + '/ws';
@@ -15,26 +21,31 @@ module.exports = function (url, options) {
   var source;
 
   var connection = reconnect(function (stream) {
+
     source = multiplex(function (stream, id) {
       //
-      // emit events for server-initated streams
+      // emit channel events for server-initated streams
       //
-      client.emit('stream', stream, id);
+      client.emit('channel', id, stream);
     });
 
     source.pipe(stream).pipe(source);
+
     client.connected = true;
     client.emit('connect');
   });
 
   client.request = function (context) {
-    var headersReceived;
-
     if (typeof context === 'string') {
       context = { url: context };
     }
 
+    var headersReceived;
     return new Promise(function (resolve, reject) {
+
+      //
+      // if not connected defer until connect
+      //
       if (!client.connected) {
         return client.once('connect', function () {
           client.request(context).then(resolve, reject);
@@ -48,53 +59,57 @@ module.exports = function (url, options) {
 
       stream
         .on('error', reject)
-        .pipe(split(/(\r?\n)/))
+        .pipe(split2(/(\r?\n)/))
         .on('data', function (data) {
-          if (!headersReceived) {
-            headersReceived = true;
-            stream.removeListener('error', reject);
+          if (headersReceived) {
+            return;
+          }
 
-            try {
-              var context = JSON.parse(data);
-              var headers = context.headers || {};
-              var mode = (headers['content-type'] || '').match(JSON_MODE_RE);
+          headersReceived = true;
+          stream.removeListener('error', reject);
 
-              //
-              // pipe through JSONStream if `stream-parsable` param set
-              //
-              if (mode && mode[2]) {
-                context.body = stream.pipe(JSONStream.parse(mode[4]));
+          var context;
+          try {
+            context = JSON.parse(data) || {};
+          }
+          catch (error) {
+            return reject(error);
+          }
+
+          var headers = context.headers || {};
+          var objMode = (headers['content-type'] || '').match(OBJ_MODE_RE);
+
+          //
+          // pipe through JSONStream if `stream-parsable` param set
+          //
+          if (objMode && objMode[2]) {
+            context.body = stream.pipe(JSONStream.parse(objMode[4]));
+            resolve(context);
+          }
+
+          //
+          // non-streaming json, collect up body and run through JSON.parse
+          //
+          else if (objMode) {
+            var collect = concat(function (data) {
+              try {
+                context.body = JSON.parse(data);
+                resolve(context);
               }
-
-              //
-              // non-streaming json, collect up body and run through JSON.parse
-              //
-              else if (mode) {
-                context.body = new Promise(function (resolve, reject) {
-                  var process = concat(function (data) {
-                    try {
-                      resolve(JSON.parse(data));
-                    }
-                    catch (error) {
-                      reject(error);
-                    }
-                  })
-                  stream.on('error', reject).pipe(process);
-                });
+              catch (error) {
+                reject(error);
               }
+            });
 
-              //
-              // pass stream along directly
-              //
-              else {
-                context.body = stream;
-              }
+            stream.once('error', reject).pipe(collect);
+          }
 
-              resolve(context);
-            }
-            catch (error) {
-              reject(error);
-            }
+          //
+          // pass stream along directly
+          //
+          else {
+            context.body = stream;
+            resolve(context);
           }
         });
 
@@ -106,23 +121,17 @@ module.exports = function (url, options) {
     });
   };
 
-  client.close = function () {
-    connection.disconnect();
-    client.request = function () {
-      throw new Error('Client closed');
-    };
-    client.emit('close');
-    connection.removeListener('disconnect', onDisconnect)
-  };
+  connection.on('error', function (error) {
+    console.log('CONN ERR', error);
+  })
 
-  function onDisconnect() {
+  connection.on('disconnect', function () {
     source = null;
     client.connected = false;
     client.emit('disconnect');
-  }
+  });
 
-  connection.on('disconnect', onDisconnect);
-  connection.once('fail', client.close);
+  // TODO: disconnect/close
 
   connection.connect(url);
 
